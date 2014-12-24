6f4b51d3d5050e27e91d1cae0b5480e6ad1e4653
taoyuan <torworx@gmail.com>
2014-12-24 10:50:09 +0800

    upgrade mosca to 0.26.2 and make example work.

--- examples/simple/server.js
+++ examples/simple/server.js
@@ -11,20 +11,20 @@ app.use(function (req, res, next) {
 });
 
 // route to mesasge for '/hello/me'
-app.route('/hello/me', function(req, res, next){
+app.all('/hello/me', function(req, res, next){
     console.log(req.foo); // will output 'hello'
     console.log('received', req.topic, req.payload);
     console.log('----------------------------------');
 });
 
 // route to mesasge for '/hello/you'
-app.route('/hello/you', function(req, res, next){
+app.all('/hello/you', function(req, res, next){
     console.log('received', req.topic, req.payload);
     console.log('----------------------------------');
 });
 
 // route to mesasge for '/some/:person/you' with a named param for that token
-app.route('/some/:person/you', function(req, res, next){
+app.all('/some/:person/you', function(req, res, next){
     console.log(req.params.person);
     console.log('received', req.topic, req.payload);
     console.log('----------------------------------');
