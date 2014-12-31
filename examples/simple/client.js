f34ba9b39c162c1d5a21a992e7f6df2ea1ba473c
taoyuan <torworx@gmail.com>
2014-12-31 11:31:59 +0800

    fixed #1 to support QOS 1

--- examples/simple/client.js
+++ examples/simple/client.js
@@ -3,7 +3,9 @@
 var mqtt = require('mqtt');
 var client = mqtt.createClient(9191);
 
-client.publish('/hello/me', 'hello_me');
+client.publish('/hello/me', 'hello_me', {qos: 1}, function () {
+    console.log("publish done with qos=1")
+});
 client.publish('/hello/you', 'hello_you');
 client.publish('/some/ty/you', 'some_ty_you');
 
